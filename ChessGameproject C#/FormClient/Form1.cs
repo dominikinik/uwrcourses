9abaf1c86d43625649fb6c9a6142e76962528431
dominikinik <130679126+dominikinik@users.noreply.github.com>
2023-11-27 10:21:18 +0100

    Add files via upload

---
+++ ChessGameproject C#/FormClient/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using Chess;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormClient
+{
+    public partial class Form1 : Form
+    {
+        ChessBoard chessBoard = new ChessBoard();
+        Chess.Point selectedPiece = new Chess.Point();
+        int selectedPlayer = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            for (int x = 1; x < boardLayoutPanel.ColumnCount; x++)
+            {
+                for (int y = 1; y < boardLayoutPanel.RowCount; y++)
+                {
+                    Button button = new Button();
+                    button.Dock = DockStyle.Fill;
+                    button.Margin = new Padding(0);
+                    button.FlatStyle = FlatStyle.Flat;
+                    button.FlatAppearance.BorderSize = 0;
+                    if ((x + y) % 2 == 1) button.BackColor = Color.LightGray;
+                    else button.BackColor = Color.DarkGray;
+                    boardLayoutPanel.Controls.Add(button);
+                    button.Click += Click_Board;
+                }
+            }
+
+            DrawPieces(chessBoard);
+        }
+
+        private void Click_Board(object s, EventArgs e)
+        {
+            DrawPieces(chessBoard);
+            if (!(s is Button)) return;
+            Button button = (Button)s;
+            button.FlatStyle = FlatStyle.Standard;
+            TableLayoutPanelCellPosition a = boardLayoutPanel.GetPositionFromControl((Control)s);
+
+            if (!(button.Tag is ChessPiece))
+            {
+                if (selectedPlayer > -1)
+                {
+                    chessBoard.MakePieceAction(selectedPiece.x, selectedPiece.y, a.Column - 1, a.Row - 1);
+                    selectedPlayer = -1;
+                    DrawPieces(chessBoard);
+                }
+                return;
+            }
+
+            ChessPiece chessPiece = (ChessPiece) button.Tag;
+            
+
+            if (selectedPlayer > -1 && selectedPlayer != chessPiece.Player)
+            {
+                chessBoard.MakePieceAction(selectedPiece.x, selectedPiece.y, a.Column - 1, a.Row - 1);
+                selectedPlayer = -1;
+                DrawPieces(chessBoard);
+            }
+            else
+            {
+                selectedPlayer = chessPiece.Player;
+                selectedPiece.x = a.Column - 1;
+                selectedPiece.y = a.Row - 1;
+                foreach (Chess.Point point in chessBoard.PosiblePieceActions(a.Column - 1, a.Row - 1))
+                {
+                    Button actionButton = (Button)boardLayoutPanel.GetControlFromPosition(point.x + 1, point.y + 1);
+                    actionButton.FlatStyle = FlatStyle.Popup;
+                   
+                }
+               
+            }
+        }
+
+        private void DrawPieces(ChessBoard board)
+        {
+            for (int x = 0; x < board.GetLength(0); x++)
+            {
+                for (int y = 0; y < board.GetLength(1); y++)
+                {
+                    Button button = (Button)boardLayoutPanel.GetControlFromPosition(x + 1, y + 1);
+                    button.FlatStyle = FlatStyle.Flat;
+                    
+                    
+                    if (board[x, y] != null)
+                    {
+                        ChessPiece chessPiece = board[x, y];
+                        button.Tag = chessPiece;
+                        button.Text = chessPiece.ToString().Replace("Chess.", "");
+                        if (chessPiece.Player == 1) button.ForeColor = Color.White;
+                        else button.ForeColor = Color.Black;
+                    }
+                    else
+                    {
+                        button.Text = "";
+                        button.Tag = null;
+                    }
+                    
+                }
+            }
+        }
+
+        private void boardLayoutPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
